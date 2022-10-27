f1dcbf66e945ae4254997979747e192536051ac8
Brandon Elefano <brandon.elefano@outlook.com>
2022-10-26 21:44:05 -0400

    Added swear filter, more responses

--- main.c
+++ main.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <ctype.h>
 #include <string.h>
+
+#define MAX_WORDS 3
+
+// J.F. Sebastian
+void make_lower(char * inp_str) {
+    for ( ; *inp_str; ++inp_str) *inp_str = tolower( (unsigned char)*inp_str );
+}
+
+
+
+void process_cmd(char ** tokens, char ** res) {
+
+    // Checking the cmd for curse words
+    for (int i = 0; i < MAX_WORDS; i++) {
+        if (tokens[i] == NULL) {
+            break;
+        } 
+        make_lower(tokens[i]);
+
+        // please don't swear, he is doing his best
+        if (strcmp(tokens[i], "fuck") == 0 || strcmp(tokens[i], "shit") == 0 || 
+            strcmp(tokens[i], "bitch") == 0 || strcmp(tokens[i], "cunt") == 0 || 
+            strcmp(tokens[i], "bastard") == 0 || strcmp(tokens[i], "whore") == 0) {
+
+            *res = malloc(28 * sizeof(char));
+            strcpy(*res, "Please don't swear at me :(");
+            return;
+        }
+    }
+
+    if (strcmp(tokens[0], "hello") == 0) {
+        *res = malloc(9 * sizeof(char));
+        strcpy(*res, "Hello :)");
+    } else if (strcmp(tokens[0], "goodbye") == 0) {
+        *res = malloc(11 * sizeof(char));
+        strcpy(*res, "Bye-bye! :)");
+    } else if (strcmp(tokens[0], "please") == 0) {
+        *res = malloc(18 * sizeof(char));
+        strcpy(*res, "Why certainly! :)");
+    } else if (strcmp(tokens[1], "please") == 0) {
+        *res = malloc(13 * sizeof(char));
+        strcpy(*res, "Naturally! :)");
+    } else if (strcmp(tokens[2], "please") == 0) {
+        *res = malloc(18 * sizeof(char));
+        strcpy(*res, "But of course! :)");
+    } else {
+        *res = malloc(16 * sizeof(char));
+        strcpy(*res, "You are rude :(");
+    }
+
+    free(tokens);
+    return;
+}
 
 char * read_input(void) {
     // https://www.dmulholl.com/lets-build/a-command-line-shell.html
@@ -9,32 +63,32 @@ char * read_input(void) {
     size_t cmd_len = 0;
     getline(&cmd, &cmd_len, stdin);
 
-    int length = 0;
-    int capacity = 4;
-    char **tokens = malloc(capacity * sizeof(char*));
+    char * res;
+
+    int num_tokens = 0;
+    char **tokens = malloc((MAX_WORDS + 1) * sizeof(char*));
 
     char *delimiters = " \t\r\n";
     char *token = strtok(cmd, delimiters);
 
     while (token != NULL) {
-        tokens[length] = token;
-        length++;
+        tokens[num_tokens] = token;
+        num_tokens++;
+
+        if (num_tokens > MAX_WORDS) {
+            res = malloc(43 * sizeof(char));
+            strcpy(res, "Too much information. Can't understand. :(");
+
+            free(tokens);
+            return res;
+        }
 
         token = strtok(NULL, delimiters);
     }
 
-    tokens[length] = NULL;
+    tokens[num_tokens] = NULL;
 
-    char * res;
-
-    if (strcmp(tokens[0], "please") != 0 && strcmp(tokens[length - 1], "please")  != 0) {
-        res = malloc(15);
-        strcpy(res, "You are rude :(");
-    } else {
-        res = malloc(9);
-        strcpy(res, "Hello! :)");
-    }
-    free(tokens);
+    process_cmd(tokens, &res);
 
     return res;
 }
@@ -42,10 +96,10 @@ char * read_input(void) {
 int main(int argc, char **argv) {
     char * res;
 
-    fprintf(stderr, "(o _ o) <(");
 
     do {
 
+        fprintf(stderr, "(o _ o) <(");
         res = read_input();
 
         if(!res) {
@@ -57,13 +111,12 @@ int main(int argc, char **argv) {
             continue;
         }
 
-        if(strcmp(res, "goodbye\n") == 0 || strcmp(res, "bye-bye\n") == 0 || strcmp(res, "see ya\n") == 0) {
+        printf("%s\n", res);
+
+        if(strcmp(res, "Bye-bye! :)") == 0) {
             free(res);
             break;
         }
-
-        printf("%s\n", res);
-
         free(res);
 
     } while(1);
